2570a129e9d9f3cae3a6ddbbbf2463ad780ec169
Afaque Ahmed <145118453+Afaque-A@users.noreply.github.com>
2024-08-15 15:26:32 +0500

    initial

---
+++ lib/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color(0xffFCEFE9C);
+const btnColor = Color(0xffE8073E);
---
+++ lib/onbording.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Onbording extends StatefulWidget {
+  const Onbording({super.key});
+
+  @override
+  State<Onbording> createState() => _OnbordingState();
+}
+
+class _OnbordingState extends State<Onbording> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
